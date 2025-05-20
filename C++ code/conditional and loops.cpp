f039dd61e549439defee2c545804e09066ef2a55
jis24 <165779138+orscoder@users.noreply.github.com>
2025-05-21 01:06:26 +0530

    lecture 3

--- C++ code/conditional and loops.cpp
+++ C++ code/conditional and loops.cpp
@@ -6,32 +6,56 @@ int main(){
     int a,b;
 
 
-    cin>> n;
+    // cin>> n;
 
-    cout<< "value of n is: "<< n <<endl;
+    // cout<< "value of n is: "<< n <<endl;
 
-    if(n>0){
-        cout<< "n is positive" <<endl;       
-    }
-    else{
-        cout<< "n is not positive" <<endl;
-    }
+    // if(n>0){
+    //     cout<< "n is positive" <<endl;       
+    // }
+    // else{
+    //     cout<< "n is not positive" <<endl;
+    // }
 
-    cout<< "Enter the value of a: ";
-    cin>> a;
+    // cout<< "Enter the value of a: ";
+    // cin>> a;
 
-    cout<< "Enter the value of b: ";
-    cin>> b;
+    // cout<< "Enter the value of b: ";
+    // cin>> b;
 
-    if(a>b){
-        cout<< "value of A is greatest"<<endl;
-    }
-    if(b>a){
-        cout<< "value of B is greatest"<<endl;
-    }
-    else{
-        cout<< "values are same"<<endl;
-    }
+    // if(a>b){
+    //     cout<< "value of A is greatest"<<endl;
+    // }
+    // if(b>a){
+    //     cout<< "value of B is greatest"<<endl;
+    // }
+    // else{
+    //     cout<< "values are same"<<endl;
+    // }
 
 
+    //loops 
+
+//code for printing 0 to n numbers
+int i=0;
+
+cout<< "Enter the value of n: ";
+cin>> n;
+while(i<=n){
+    cout<< i<< " + ";
+    i++;
+}
+cout<<endl;
+
+
+
+//code for sum of n numbers 
+i=0;
+int sum=0;
+cout<< "the sum of n numbers are: ";
+while(i<=n){
+    sum = sum + i;
+    i++;
+}
+cout<< sum <<endl;
 }
