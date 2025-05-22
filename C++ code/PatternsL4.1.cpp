ba1b81e926ed535383342fd072e3d7b63803e236
jis24 <165779138+orscoder@users.noreply.github.com>
2025-05-22 23:59:29 +0530

    lecture 4
    in this we had printed the pattern which contain the value of J in reverse in each row we achieve it using the formula n-j+1 the patterns is 321...\n..

--- C++ code/PatternsL4.1.cpp
+++ C++ code/PatternsL4.1.cpp
@@ -4,14 +4,37 @@ using namespace std;
 int main(){
 
     int n,i=1,j=1;
-
+    
     cout<<"Enter size of determinant: ";
     cin>>n;
+
+//print the pattern :
+// 1 2 3
+// 1 2 3
+// 1 2 3
+/*
 
     while(i<=n){
         j=1;
         while(j<=n){
             cout<<j;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+cout<<endl;
+*/
+
+//print the pattern : using the formula
+//3 2 1
+//3 2 1
+//3 2 1
+    while(i<=n){
+        j=1;
+        while(j<=n){
+            cout<<n-j+1<<" ";
             j++;
         }
         cout<<endl;
