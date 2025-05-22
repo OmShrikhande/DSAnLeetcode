b8dc257c135a26a45418761f48086390eafed412
jis24 <165779138+orscoder@users.noreply.github.com>
2025-05-23 00:57:55 +0530

    L4.2.4.1 L4.2.4.2 L4.2.5

--- C++ code/PatternsL4_2.cpp
+++ C++ code/PatternsL4_2.cpp
@@ -55,9 +55,11 @@ To print the pattern:
 
 /*
 to print the pattern:
+1
+2 3
+4 5 6
+7 8 9 10
 
-
-*/
 int count =1;
     while(i<n){
 
@@ -72,10 +74,72 @@ int count =1;
     }
 
 
+*/
+
+
+
+/*
+to print the pattern:
+1 
+2 3
+3 4 5
+4 5 6 7
+5 6 7 8 9
+
+
+while(i<n){
+    j=0;
+    int value=i+1;
+
+    while(j<=i){
+        cout<<value<<" ";
+        value++;
+        j++;
+    }
+    cout<<endl;
+    i++;
+}
+
+
+** Another way to print the same pattern is: **
+while(i<n){
+    j=0;
+    while(j<=i){
+        cout<<i+j+1<<" ";
+        j++;
+    }
+    cout<<endl;
+    i++;
+
+}
+
+*/
 
 
 
 
+
+
+
+/* To print the pattern :
+Enter size of determinant: 5
+1 
+2 1
+3 2 1
+4 3 2 1
+5 4 3 2 1
+
+
+while(i<n){
+    j=0;
+    while(j<=i){
+        cout<<i-j+1<<" "; // in this the formula is used is i-j+1
+        j++;
+    }
+    cout<<endl;
+    i++;
+}
+*/
 
 
 
